7957abf9e25ab1817f8093424b9b3272b4a1309f
syedmubarak47 <syedmubarak13005@gmail.com>
2024-08-01 13:06:35 +0530

    Create matrix.py

---
+++ condition statement/matrix.py
@@ -0,0 +1,12 @@
+row1=[1,1,1]
+row2=[1,1,1]
+row3=[1,1,1]
+matrix=[row1,row2,row3]
+print(f"{row1}\n{row2}\n{row3}")
+position=input("enter the position where you want to hide money:")
+#32
+row_number=int(position[0])
+column_number=int(position[1])
+row_selected=matrix[row_number-1]
+row_selected[column_number-1]="x"
+print(f"{row1}\n{row2}\n{row3}")
